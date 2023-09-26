78048b13dcb79b2da50aa033f9531e78b4f31b18
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-09-26 18:29:39 +0530

    Modules

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+const port= process.env.PORT || 8000;
 
-app.get("/students", (req, res)=>{
-    
+app.post("/students", (req, res)=>{
+    res.send("hello from the other side")
+})
+
+app.listen(port, ()=>{
+    console.log(`connection is setup at ${port}`);
 })
