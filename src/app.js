34fb36eea00fb8e7e5a2ab850faa0d6102af72a1
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-09-27 01:02:56 +0530

    .

--- src/app.js
+++ src/app.js
@@ -4,10 +4,23 @@ const student= require("./models/student");
 const app = express();
 const port= process.env.PORT || 8000;
 
+app.use(express.json());
+
 // create a new student
-app.post("/students", (req, res)=>{
+app.post("/student", (req, res)=>{
+
+    console.log(req.body);
+    const user = new student(req.body)
+    user.save().then(()=>{
+        res.send(user);
+    }).catch((e)=>{
+        res.send(e);
+    }) 
+
     res.send("hello from the other side")
 })
+
+
 
 app.listen(port, ()=>{
     console.log(`connection is setup at ${port}`);
