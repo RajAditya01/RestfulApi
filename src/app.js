80520a58b2e3e76da28574caabfe7d84e71a74a5
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-09-26 18:31:16 +0530

    .

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port= process.env.PORT || 8000;
 
+app.get("/", (req, res)=>{
+    res.send("hello from the Home side")
+})
+
 app.post("/students", (req, res)=>{
     res.send("hello from the other side")
 })
